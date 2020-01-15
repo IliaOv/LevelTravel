4736b58790451938f464e739ffb0ae003d6bbe9c
IliaOv <ilya.ovchinniko@gmail.com>
2020-01-15 17:28:10 +0300

    slider start

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -6,14 +6,23 @@ import russia from "../../static/russia1.json";
 class Filter extends Component {
   render() {
     return (
-      <React.Fragment>
+      <div className={"filter"}>
         <Search
           items={russia}
           placeholder="Выберите город"
           maxSelected={1}
           multiple={false}
         />
-      </React.Fragment>
+
+        <div className={"square"}>
+          <span className={"plus"}>+</span>
+        </div>
+
+        <div className="slider">
+          <p>Где сейчас теплее, чем</p>
+          <input type="range" min="-10" max="20" step="1" />
+        </div>
+      </div>
     );
   }
 }
