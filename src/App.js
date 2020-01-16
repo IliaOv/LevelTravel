69b0911f93a80465bd06e18dd2039bbfc6b5ad92
IliaOv <ilya.ovchinniko@gmail.com>
2020-01-16 09:40:08 +0300

    card ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Filter } from "./components/Filter/Filter.js";
 import { Card } from "./components/Card/Card.js";
+import russia from "../src/static/russia.json";
+import sun from "../src/static/img/sun.png";
+import cloud from "../src/static/img/cloud.png";
+import rain from "../src/static/img/rain.png";
 import "./App.css";
 
 class App extends React.Component {
@@ -11,7 +15,22 @@ class App extends React.Component {
           <Filter />
         </header>
         <main>
-          <Card />
+          {russia.map((item, index) => (
+            <Card
+              key={Math.random()}
+              city={item["title"]}
+              img={
+                item["icon"] === "cloud"
+                  ? cloud
+                  : item["icon"] === "sun"
+                  ? sun
+                  : rain
+              }
+              temp={item["temp"]}
+              wind={item["wind"]}
+              pressure={item["pressure"]}
+            />
+          ))}
         </main>
       </React.Fragment>
     );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,18 +3,25 @@ import "./Card.scss";
 
 class Card extends React.Component {
   render() {
+    const { city, img, temp, wind, pressure } = this.props;
     return (
-        <div className={'card'}>
-          <h2 className={'card__header'}>Moscow</h2>
-          <div className={'card_shortinf'}>
-            <p>img</p>
-            <p className={'card_temp'}>+7 °C</p>
-          </div>
-          <div className={'card__longinf'}>
-            <p className={'card_wind'}>Ветер:</p>
-            <p className={'card_pressure'}>Давление:</p>
-          </div>
+      <div className={"card"}>
+        <h2 className={"card__header"}>{city}</h2>
+        <div className={"card_shortinf"}>
+          <img
+            className={"card__img"}
+            src={img}
+            width={52}
+            height={52}
+            alt={"Иконка погоды"}
+          />
+          <p className={"card_temp"}>{temp} °C</p>
         </div>
+        <div className={"card__longinf"}>
+          <p className={"card_wind"}>Ветер: {wind}</p>
+          <p className={"card_pressure"}>Давление: {pressure}</p>
+        </div>
+      </div>
     );
   }
 }
