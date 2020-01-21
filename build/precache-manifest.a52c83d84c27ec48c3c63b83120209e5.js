fcd48ea230b1d581014924d24b1d5543c5c4713a
IliaOv <ilya.ovchinniko@gmail.com>
2020-01-21 07:05:21 +0300

    bug fix

---
+++ build/precache-manifest.a52c83d84c27ec48c3c63b83120209e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a442dc725f3f3afc94b00aa695ad6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca3c4589b169f54e36e6",
+    "url": "./static/css/main.78444c6f.chunk.css"
+  },
+  {
+    "revision": "85c658a093d865ed2dde",
+    "url": "./static/js/2.8bc6912a.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "./static/js/2.8bc6912a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca3c4589b169f54e36e6",
+    "url": "./static/js/main.bb13a429.chunk.js"
+  },
+  {
+    "revision": "353824c915518b56d5bd",
+    "url": "./static/js/runtime-main.1130748e.js"
+  }
+]);
